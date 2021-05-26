612b24aabacdb3afc2ec75062875fda0f3b51be0
karishmasharma3 <53438501+karishmasharma3@users.noreply.github.com>
2021-05-26 18:03:08 +0530

    first commit

---
+++ UPSTAC-week-3/Documents/HelloWorld.java
@@ -0,0 +1,27 @@
+/* for git test purpose*/
+class HelloWorld {
+
+
+
+
+	public static void main(String[] args) {
+		
+		for (int num = 2; num <= 1000; num++) {
+			boolean isPrime = true;
+                        
+			for (int i = 2; i <= Math.sqrt(num); i++) {  
+		        
+                      
+
+			                if (num % i == 0) { 
+					isPrime = false;
+					break;
+				}
+			}
+
+			if (isPrime == true) {
+				System.out.println(num);
+			}
+		}
+}
+}
